495874bdce7eeeb288e0a3db52daffd137222395
hujinbin <437044048@qq.com>
2020-01-11 15:27:07 +0800

    edit

--- config/config.js
+++ config/config.js
@@ -3,11 +3,11 @@
 module.exports = {
     backendUrl:'http://127.0.0.1:3000',  //后台接口地址
     options: {  // mongodb数据库
-        db_user: "jueAdmin",
-        db_pwd: "!Ahjb102728",
-        db_host: "106.14.202.220",
+        db_user: "admin",
+        db_pwd: "123456",
+        db_host: "localhost",
         db_port: 27017,
-        db_name: "jue"
+        db_name: "novel"
     },
     mysql:{ // mysql数据库配置
         host: 'localhost',
