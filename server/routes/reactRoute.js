743d98ce59362e41a1d804c1b11166ebe82ded90
hujinbin <437044048@qq.com>
2020-01-18 16:13:49 +0800

    edit

--- server/routes/reactRoute.js
+++ server/routes/reactRoute.js
@@ -8,18 +8,19 @@ import { RoutesIndex, routes } from '../../src/router/index.jsx';
 import getData from '../../src/common/getData';
 
 async function reactRoute(ctx, next) {
+    const url = ctx.request.path
     for (let item of routes) {
         let itemPath = item.path
         // 判断是否有子页面 统一/:id处理
         if(String(item.path).indexOf('/:id') != -1){
            let path = String(item.path).match(/\/(\S*)\//)[1]
-           const urlParamsList = String(ctx.url).split('/')
+           const urlParamsList = String(url).split('/')
            if(urlParamsList.length ===3 && urlParamsList[1] === path){
-            itemPath = ctx.url
+            itemPath = url
            }
         }
-        if (itemPath == ctx.url) {
-            const data = await getData(ctx.url);
+        if (itemPath == url) {
+            const data = await getData(ctx.request.path);
             await ctx.render('index', {
                 title: item.name ? item.name+ '-' : '',
                 root: renderToStaticMarkup(
