8abc8c36619441cf1a149579d45b1c0933a13779
hujinbin <437044048@qq.com>
2020-03-31 17:33:04 +0800

    koa文件接收参数问题

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 import Koa from 'koa';
 import json from 'koa-json';
-import bodyParser from 'koa-bodyparser';
+import body from 'koa-body';
 import logger from 'koa-logger';
 import session from 'koa-session';
 import compress from 'koa-compress';
@@ -10,7 +10,12 @@ import cors from 'koa2-cors';
 const app = new Koa();
 app.use(convert(session(app)));
 app.use(compress());
-app.use(bodyParser());
+app.use(body({
+    multipart: true,
+    formidable: {
+        maxFileSize: 800*1024*1024    // 设置上传文件大小最大限制，默认8M
+    }
+}));
 app.use(cors());
 app.use(json());
 app.use(logger());
